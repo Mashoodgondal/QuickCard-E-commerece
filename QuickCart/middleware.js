4870a01dd31beb3fa9a9bb0ed4b6d5a559b1dc19
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-08-17 20:44:06 +0500

    add clerk

--- QuickCart/middleware.js
+++ QuickCart/middleware.js
@@ -39,3 +39,7 @@ export const config = {
 //         // Always run for API routes
 //         '/(api|trpc)(.*)',
 
+// Skip Next.js internals and all static files, unless found in search params
+'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
