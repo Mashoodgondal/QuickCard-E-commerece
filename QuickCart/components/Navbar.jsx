96457ba663ea2f4b52156252927a5adf9a5211cb
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-08-17 20:32:06 +0500

    add clerk

--- QuickCart/components/Navbar.jsx
+++ QuickCart/components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react";
-import { assets} from "@/assets/assets";
+import { assets } from "@/assets/assets";
 import Link from "next/link"
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
@@ -42,7 +42,6 @@ const Navbar = () => {
           Account
         </button>
       </ul>
-
       <div className="flex items-center md:hidden gap-3">
         {isSeller && <button onClick={() => router.push('/seller')} className="text-xs border px-4 py-1.5 rounded-full">Seller Dashboard</button>}
         <button className="flex items-center gap-2 hover:text-gray-900 transition">
