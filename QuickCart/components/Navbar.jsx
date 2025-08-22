e0f6a4831b8a2eba7c23f746f26a5decdfa7e767
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-08-22 22:56:37 +0500

    clerk authentication

--- QuickCart/components/Navbar.jsx
+++ QuickCart/components/Navbar.jsx
@@ -98,3 +98,11 @@ export default Navbar;
 //             href="/sign-up"
 //             className="flex items-center gap-2 border px-3 py-1.5 rounded-full hover:bg-gray-100 transition"
 //           ></Link>
+
+//  <Link <Link
+//   href="/sign-up"
+//   className="flex items-center gap-2 border px-3 py-1.5 rounded-full hover:bg-gray-100 transition"
+// >
+//             href="/sign-up"
+//             className="flex items-center gap-2 border px-3 py-1.5 rounded-full hover:bg-gray-100 transition"
+// 
