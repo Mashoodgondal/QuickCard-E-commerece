610c72d2977b6d476eef6caf8304c01e0fd9ca3b
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-08-23 14:22:04 +0500

    add api key from clerk

--- QuickCart/components/Navbar.jsx
+++ QuickCart/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
-      {/* Logo */}
+
       <Image
         className="cursor-pointer w-28 md:w-32"
         onClick={() => router.push('/')}
@@ -21,7 +21,7 @@ const Navbar = () => {
         alt="logo"
       />
 
-      {/* Desktop Menu */}
+
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
         <Link href="/" className="hover:text-gray-900 transition">Home</Link>
         <Link href="/all-products" className="hover:text-gray-900 transition">Shop</Link>
@@ -37,11 +37,11 @@ const Navbar = () => {
         )}
       </div>
 
-      {/* Right Side Actions */}
+
       <ul className="hidden md:flex items-center gap-4">
         <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
 
-        {/* Show UserButton if logged in, otherwise show Create Account */}
+
         <SignedIn>
           <UserButton />
         </SignedIn>
@@ -56,7 +56,7 @@ const Navbar = () => {
         </SignedOut>
       </ul>
 
-      {/* Mobile Menu */}
+
       <div className="flex items-center md:hidden gap-3">
         {isSeller && (
           <button
