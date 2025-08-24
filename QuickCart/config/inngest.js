937d885b3b791a2c44b9c6e51194110ed1dbd8bc
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-08-24 17:26:33 +0500

    add inngest in project

--- QuickCart/config/inngest.js
+++ QuickCart/config/inngest.js
@@ -25,3 +25,20 @@ export const sycnUserCreation = inngest.createFunction(
         await User()
     }
 )
+
+// ingest funciton to update user data 
+export const syncUserUpdation = inngest.createFunction(
+    { id: 'update-user-from-clerk' },
+    { event: 'clerk/user.updated' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_addresses,
+            name: first_name + ' ' + last_name,
+            inage: image_url
+        }
+        await connectDB()
+        await User.findByIdAndUpdate(id, userData)
+    }
+)
